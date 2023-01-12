9a338294714f174525c0d60a025b0f6e1d549a19
João <joaoklvf@gmail.com>
2023-01-12 11:19:50 -0300

    Creating BR date-picker

---
+++ src/app/components/br-date-picker/br-date-picker.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BrDatePickerComponent } from './br-date-picker.component';
+
+describe('BrDatePickerComponent', () => {
+  let component: BrDatePickerComponent;
+  let fixture: ComponentFixture<BrDatePickerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BrDatePickerComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BrDatePickerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/br-date-picker/br-date-picker.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+
+// Depending on whether rollup is used, moment needs to be imported differently.
+// Since Moment.js doesn't have a default export, we normally need to import using the `* as`
+// syntax. However, rollup creates a synthetic default module and we thus need to import it using
+// the `default as` syntax.
+import * as moment from 'moment';
+// tslint:disable-next-line:no-duplicate-imports
+
+// See the Moment.js docs for the meaning of these formats:
+// https://momentjs.com/docs/#/displaying/format/
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'DD/MM/YYYY',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
+
+@Component({
+  selector: 'app-br-date-picker',
+  templateUrl: './br-date-picker.component.html',
+  styleUrls: ['./br-date-picker.component.scss'],
+  providers: [
+    // `MomentDateAdapter` can be automatically provided by importing `MomentDateModule` in your
+    // application's root module. We provide it at the component level here, due to limitations of
+    // our example generation script.
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+  ],
+})
+export class BrDatePickerComponent {
+  date = new FormControl(moment());
+}
