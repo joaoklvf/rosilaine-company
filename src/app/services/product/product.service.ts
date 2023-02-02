14995ec68ee6a849645958854013dbecb23c3bf7
João Figueredo <joaoklvf@gmail.com>
2023-02-01 23:01:29 -0300

    Adding product service

---
+++ src/app/services/product/product.service.ts
@@ -0,0 +1,117 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, tap, map, of } from 'rxjs';
+import { Product } from 'src/app/models/product';
+import { MessageService } from '../message/message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+  private productsUrl = `http://localhost:3001/api/products`;// URL to web api
+
+  /** GET products from the server */
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productsUrl)
+      .pipe(
+        tap(_ => this.log('fetched products')),
+        catchError(this.handleError<Product[]>('getProducts', []))
+      );
+  }
+
+  /** GET customer by id. Return `undefined` when id not found */
+  getProductNo404<Data>(id: number): Observable<Product> {
+    const url = `${this.productsUrl}/?id=${id}`;
+    return this.http.get<Product[]>(url)
+      .pipe(
+        map(products => products[0]), // returns a {0|1} element array
+        tap(h => {
+          const outcome = h ? 'fetched' : 'did not find';
+          this.log(`${outcome} customer id=${id}`);
+        }),
+        catchError(this.handleError<Product>(`getProduct id=${id}`))
+      );
+  }
+
+  /** GET customer by id. Will 404 if id not found */
+  getProduct(id: number): Observable<Product> {
+    const url = `${this.productsUrl}/${id}`;
+    return this.http.get<Product>(url).pipe(
+      tap(_ => this.log(`fetched customer id=${id}`)),
+      catchError(this.handleError<Product>(`getProduct id=${id}`))
+    );
+  }
+
+  /* GET products whose name contains search term */
+  searchProducts(term: string): Observable<Product[]> {
+    if (!term.trim()) {
+      // if not search term, return empty customer array.
+      return of([]);
+    }
+    return this.http.get<Product[]>(`${this.productsUrl}/?name=${term}`).pipe(
+      tap(x => x.length ?
+        this.log(`found products matching "${term}"`) :
+        this.log(`no products matching "${term}"`)),
+      catchError(this.handleError<Product[]>('searchProducts', []))
+    );
+  }
+
+  //////// Save methods //////////
+
+  /** POST: add a new customer to the server */
+  addProduct(customer: Product): Observable<Product> {
+    console.log(customer, 'customer');
+    return this.http.post<Product>(this.productsUrl, customer).pipe(
+      tap((newProduct: Product) => this.log(`added customer w/ id=${newProduct.id}`)),
+      catchError(this.handleError<Product>('addProduct'))
+    );
+  }
+
+  /** DELETE: delete the customer from the server */
+  deleteProduct(id: number): Observable<Product> {
+    const url = `${this.productsUrl}/${id}`;
+
+    return this.http.delete<Product>(url).pipe(
+      tap(_ => this.log(`deleted customer id=${id}`)),
+      catchError(this.handleError<Product>('deleteProduct'))
+    );
+  }
+
+  /** PUT: update the customer on the server */
+  updateProduct(customer: Product): Observable<any> {
+    return this.http.put(`${this.productsUrl}/${customer.id}`, customer).pipe(
+      tap(_ => this.log(`updated customer id=${customer.id}`)),
+      catchError(this.handleError<any>('updateProduct'))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a ProductService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`ProductService: ${message}`);
+  }
+}
