2f9aa3178ca4ed5390150c5c21e144d075df1a1e
João Figueredo <joaoklvf@gmail.com>
2023-02-01 23:02:31 -0300

    creating generic list component

--- src/app/components/generic-list/generic-list.component.ts
+++ src/app/components/generic-list/generic-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { formatMap, FormatOptions } from 'src/app/interfaces/format';
 import { ColumnProps } from './interfaces';
 
@@ -11,10 +11,18 @@ export class GenericListComponent<T> {
   @Input() data: T[] = [];
   @Input() columnsProps: ColumnProps<T>[] = [];
   @Input() title: string = '';
-  @Input() editAction: (item: T) => void = () => { };
-  @Input() deleteAction: (item: T) => void = () => { };
+  @Output() editAction = new EventEmitter<T>();
+  @Output() deleteAction = new EventEmitter<T>();
 
   formatValue(value: any, formatOption: FormatOptions = FormatOptions.Default) {
     return formatMap[formatOption](value);
+  }
+
+  update(value: T) {
+    this.editAction.emit(value);
+  }
+
+  remove(value: T) {
+    this.deleteAction.emit(value);
   }
 }
