8ad81e14d22753500d7b760cf3491696a8f85313
João Figueredo <joaoklvf@gmail.com>
2023-02-01 23:03:24 -0300

    Invoking generic list and in the components

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -1,21 +1,48 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormatOptions } from 'src/app/interfaces/format';
 import { Customer } from 'src/app/models/customer';
 import { Order } from 'src/app/models/order';
 import { Product } from 'src/app/models/product';
+import { CustomerService } from 'src/app/services/customer/customer.service';
 import { OrderService } from 'src/app/services/order/order.service';
+import { ProductService } from 'src/app/services/product/product.service';
+import { ColumnProps } from '../generic-list/interfaces';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.scss']
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
   order = new Order();
   product = new Product();
-
+  customers: Customer[] = [];
+  databaseProducts: Product[] = [];
+  columnsProps: ColumnProps<Product>[] = [
+    { key: 'description', label: 'Produto' },
+    { key: 'amount', label: 'Quantidade' },
+    { key: 'price', label: 'Preço', formatOption: FormatOptions.Currency },
+    { key: 'total', label: 'Total', formatOption: FormatOptions.Currency },
+  ];
   @ViewChild("productDescription") myInputField: ElementRef = new ElementRef(null);
 
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService, private customerService: CustomerService, private productService: ProductService) { }
+
+  ngOnInit() {
+    this.customerService.getCustomers()
+      .subscribe(customers => this.customers = customers);
+    this.productService.getProducts()
+      .subscribe(products => this.databaseProducts = products);
+  }
+
+  getOptionLabelCustomer(customer: Customer | null) {
+    return customer?.name || '';
+  }
+
+  getOptionLabelProduct(customer: Product | null) {
+    return customer?.description || '';
+  }
+
   add(): void {
     const product = {
       ...this.product,
@@ -75,7 +102,11 @@ export class OrderComponent {
     this.order.orderDate = value;
   }
 
-  setCustomer(value: any) {
-    this.order.customer = value;
+  setCustomer(customer: Customer) {
+    this.order.customer = customer;
+  }
+
+  setProduct(product: Product) {
+    this.product = product;
   }
 }
