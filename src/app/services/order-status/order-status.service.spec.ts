cfc1e713b41761c1d32bb6fbd7b26610c094d5a5
João V <joaoklvf@gmail.com>
2025-04-16 09:14:03 -0300

    fix: order status service test

--- src/app/services/order-status/order-status.service.spec.ts
+++ src/app/services/order-status/order-status.service.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-
-import { OrderStatusService } from '../../order-status.service';
+import { OrderStatusService } from './order-status.service';
 
 describe('OrderStatusService', () => {
   let service: OrderStatusService;
