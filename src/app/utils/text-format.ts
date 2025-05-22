c19497ecd74c8c96175b874b1ee66c6f8cb0b486
João Figueredo <joaoklvf@gmail.com>
2025-05-22 10:28:59 -0300

    feat: preparing getBrDateStr to receive string

--- src/app/utils/text-format.ts
+++ src/app/utils/text-format.ts
@@ -1,8 +1,8 @@
 export const getBrCurrencyStr = (value: number | string) =>
   Number(value).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
 
-export const getBrDateStr = (value: Date) =>
-  value.toLocaleDateString('pt-BR');
+export const getBrDateStr = (value: Date | string) =>
+  new Date(value).toLocaleDateString('pt-BR');
 
 export const getAmountStr = (value: number) =>
   String(value).padStart(2, "0");
