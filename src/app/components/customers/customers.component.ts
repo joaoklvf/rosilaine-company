a9dc8f14b18bfc1f5e5d994bb75af213a9e0ea89
João Figueredo <joaoklvf@gmail.com>
2025-05-22 13:29:49 -0300

    feat: using data table in customers page

--- src/app/components/customers/customers.component.ts
+++ src/app/components/customers/customers.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { Customer } from 'src/app/models/customer/customer';
 import { CustomerService } from 'src/app/services/customer/customer.service';
 import { getBrDateStr } from 'src/app/utils/text-format';
+import { DataTableComponent } from "../data-table/data-table.component";
+import { ColumnProp, FormatValueOptions } from 'src/app/interfaces/data-table';
 
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.scss'],
-  imports: [MatIconModule, RouterModule]
+  imports: [MatIconModule, RouterModule, DataTableComponent]
 })
 
 export class CustomersComponent implements OnInit {
   customers: Customer[] = [];
+  columns: ColumnProp<Customer>[] = [
+    { description: "Nome", fieldName: "name", width: '50%' },
+    { description: "Telefone", fieldName: "phone" },
+    { description: "Data de nascimento", fieldName: "birthDate", formatValue: FormatValueOptions.Date },
+  ]
 
-  constructor(private customerService: CustomerService) { }
+  constructor(private customerService: CustomerService, private router: Router) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -31,6 +38,10 @@ export class CustomersComponent implements OnInit {
   }
 
   remove() {
+    console.log('teste')
+  }
 
+  edit(value: Customer) {
+    this.router.navigate([`/customer/${value.id}`])
   }
 }
