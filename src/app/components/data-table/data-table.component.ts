8074c71c7217c3d40077fa68f397c0678e91e5b2
João Figueredo <joaoklvf@gmail.com>
2025-05-23 12:54:26 -0300

    feat: table search initial commit

--- src/app/components/data-table/data-table.component.ts
+++ src/app/components/data-table/data-table.component.ts
@@ -1,20 +1,27 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, OnInit, output } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 import { ColumnProp, } from 'src/app/interfaces/data-table';
 import { getCellValue } from 'src/app/utils/data-table-format';
 
 @Component({
   selector: 'app-data-table',
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatSelectModule, ReactiveFormsModule],
   templateUrl: './data-table.component.html',
   styleUrl: './data-table.component.scss'
 })
 
-export class DataTableComponent<T> {
+export class DataTableComponent<T> implements OnInit {
   readonly columns = input.required<ColumnProp<T>[]>();
   readonly data = input.required<T[]>();
   readonly removeAction = output<T>();
   readonly editAction = output<T>();
+  searchParameterControl: FormControl<string> = new FormControl();
+
+  ngOnInit(): void {
+    this.searchParameterControl.setValue("Cliente");
+  }
 
   remove(value: T) {
     this.removeAction.emit(value);
