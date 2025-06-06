bb28c193118789de13fd337d102211b0566861c2
João Figueredo <joaoklvf@gmail.com>
2025-06-06 15:52:22 -0300

    fix: lazy component loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,16 @@ import { HomeComponent } from "./routes/home/home.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'orders', loadChildren: () => import('./routes/order/orders-page/orders-page.component').then(x => x.OrdersPageComponent) },
-  { path: 'orders/create', loadChildren: () => import('./routes/order/order-create/order-create.component').then(x => x.OrderCreateComponent) },
-  { path: 'order/:id', loadChildren: () => import('./routes/order/order-create/order-create.component').then(x => x.OrderCreateComponent) },
-  { path: 'customers', loadChildren: () => import('./routes/customers/customers-page/customers-page.component').then(x => x.CustomersPageComponent) },
-  { path: 'customers/create', loadChildren: () => import('./routes/customers/customer-create/customer-create.component').then(x => x.CustomerCreateComponent) },
-  { path: 'customer/:id', loadChildren: () => import('./routes/customers/customer-create/customer-create.component').then(x => x.CustomerCreateComponent) },
-  { path: 'products', loadChildren: () => import('./routes/products/products.component').then(x => x.ProductsComponent) },
-  { path: 'order-status', loadChildren: () => import('./routes/order-status/order-status.component').then(x => x.OrderStatusComponent) },
-  { path: 'order-item-status', loadChildren: () => import('./routes/order-item-status/order-item-status.component').then(x => x.OrderItemStatusComponent) },
-  { path: 'customer-tags', loadChildren: () => import('./routes/customer-tags/customer-tags.component').then(x => x.CustomerTagsComponent) },
+  { path: 'orders', loadComponent: () => import('./routes/order/orders-page/orders-page.component').then(x => x.OrdersPageComponent) },
+  { path: 'orders/create', loadComponent: () => import('./routes/order/order-create/order-create.component').then(x => x.OrderCreateComponent) },
+  { path: 'order/:id', loadComponent: () => import('./routes/order/order-create/order-create.component').then(x => x.OrderCreateComponent) },
+  { path: 'customers', loadComponent: () => import('./routes/customers/customers-page/customers-page.component').then(x => x.CustomersPageComponent) },
+  { path: 'customers/create', loadComponent: () => import('./routes/customers/customer-create/customer-create.component').then(x => x.CustomerCreateComponent) },
+  { path: 'customer/:id', loadComponent: () => import('./routes/customers/customer-create/customer-create.component').then(x => x.CustomerCreateComponent) },
+  { path: 'products', loadComponent: () => import('./routes/products/products.component').then(x => x.ProductsComponent) },
+  { path: 'order-status', loadComponent: () => import('./routes/order-status/order-status.component').then(x => x.OrderStatusComponent) },
+  { path: 'order-item-status', loadComponent: () => import('./routes/order-item-status/order-item-status.component').then(x => x.OrderItemStatusComponent) },
+  { path: 'customer-tags', loadComponent: () => import('./routes/customer-tags/customer-tags.component').then(x => x.CustomerTagsComponent) },
 ];
 
 @NgModule({
