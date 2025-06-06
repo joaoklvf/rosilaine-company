f0c2fd3599f7bcfee781b68a141c0bce1626ec02
João Figueredo <joaoklvf@gmail.com>
2025-06-06 09:29:13 -0300

    feat: making installment management independent

--- src/app/routes/order/order-create/installments/installment-management/installment-management.component.ts
+++ src/app/routes/order/order-create/installments/installment-management/installment-management.component.ts
@@ -9,10 +9,11 @@ import { FormsModule } from '@angular/forms';
 import { BrDatePickerComponent } from 'src/app/components/br-date-picker/br-date-picker.component';
 import { OrderInstallmentService } from 'src/app/services/order/order-installment/order-installment.service';
 import { SnackBarService } from 'src/app/services/snack-bar/snack-bar.service';
+import { tap, catchError, of } from 'rxjs';
 
 interface ModalProps {
   order: Order;
-  saveOrder: (order: Order) => void
+  saveOrder: (order: Order) => void,
 }
 
 @Component({
@@ -25,11 +26,13 @@ interface ModalProps {
 export class InstallmentManagementComponent implements OnInit {
   data: ModalProps = inject(MAT_DIALOG_DATA);
   readonly dialogRef = inject(MatDialogRef<InstallmentManagementComponent>);
+  installments!: OrderInstallment[];
 
   constructor(private orderInstallmentService: OrderInstallmentService, private snackBarService: SnackBarService) { }
 
   ngOnInit(): void {
     this.data.order.firstInstallmentDate = this.data.order.firstInstallmentDate ?? this.data.order.installments![0].debitDate;
+    this.installments = this.data.order.installments!.map(x => ({ ...x }));
   }
 
   getBrDate = (value: Date | null) =>
@@ -59,10 +62,16 @@ export class InstallmentManagementComponent implements OnInit {
   }
 
   saveInstallments() {
-    this.orderInstallmentService.updateMany(this.data.order.installments!)
-      .subscribe(_ => {
-        this.snackBarService.success('Parcelas atualizadas com sucesso');
-        this.dialogRef.close();
-      });
+    this.orderInstallmentService.updateMany(this.installments)
+      .pipe(
+        tap(_ => {
+          const installments = this.installments.map(x => ({ ...x }));
+          this.data.saveOrder({ ...this.data.order, installments })
+          this.snackBarService.success('Parcelas atualizadas com sucesso');
+          this.dialogRef.close();
+        }),
+        catchError(() => of(this.snackBarService.error('Falha ao atualizar parcelas')))
+      )
+      .subscribe();
   }
 }
