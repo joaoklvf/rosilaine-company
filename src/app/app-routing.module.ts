4936ba0423a55a34775c53aae125e51a4e200c00
João Figueredo <joaoklvf@gmail.com>
2025-06-06 15:03:36 -0300

    feat: using home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { OrdersPageComponent } from "./routes/order/orders-page/orders-page.component";
+import { HomeComponent } from "./routes/home/home.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/orders', pathMatch: 'full' },
-  { path: 'orders', component: OrdersPageComponent },
-  { path: 'orders/create',loadChildren: () => import('./routes/order/order-create/order-create.component').then(x => x.OrderCreateComponent) },
+  { path: '', component: HomeComponent },
+  { path: 'orders', loadChildren: () => import('./routes/order/orders-page/orders-page.component').then(x => x.OrdersPageComponent) },
+  { path: 'orders/create', loadChildren: () => import('./routes/order/order-create/order-create.component').then(x => x.OrderCreateComponent) },
   { path: 'order/:id', loadChildren: () => import('./routes/order/order-create/order-create.component').then(x => x.OrderCreateComponent) },
   { path: 'customers', loadChildren: () => import('./routes/customers/customers-page/customers-page.component').then(x => x.CustomersPageComponent) },
   { path: 'customers/create', loadChildren: () => import('./routes/customers/customer-create/customer-create.component').then(x => x.CustomerCreateComponent) },
