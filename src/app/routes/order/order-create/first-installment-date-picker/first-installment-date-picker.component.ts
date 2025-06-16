f8d2ad2c69b115a3775fb1eff32b23f65313c763
João Figueredo <joaoklvf@gmail.com>
2025-06-16 19:00:13 -0300

    feat: creating first installment date picker component

---
+++ src/app/routes/order/order-create/first-installment-date-picker/first-installment-date-picker.component.ts
@@ -0,0 +1,60 @@
+import { Component, inject, input, OnInit, output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { BrDatePickerComponent } from 'src/app/components/br-date-picker/br-date-picker.component';
+import { CustomDialogComponent } from 'src/app/components/custom-dialog/custom-dialog.component';
+import { Order } from 'src/app/models/order/order';
+import { OrderService } from 'src/app/services/order/order.service';
+import { SnackBarService } from 'src/app/services/snack-bar/snack-bar.service';
+
+@Component({
+  selector: 'app-first-installment-date-picker',
+  imports: [BrDatePickerComponent],
+  templateUrl: './first-installment-date-picker.component.html',
+  styleUrl: './first-installment-date-picker.component.scss'
+})
+
+export class FirstInstallmentDatePickerComponent implements OnInit {
+  readonly order = input.required<Order>();
+  readonly saveOrderAction = output<Order>();
+  readonly dialog = inject(MatDialog);
+  firstInstallmentDate: Date | null = null;
+  constructor(private orderService: OrderService, private snackBarService: SnackBarService) { }
+
+  get _order() {
+    return this.order();
+  }
+
+  ngOnInit(): void {
+    this.firstInstallmentDate = this._order.firstInstallmentDate ?? this._order.installments?.at(0)?.paymentDate ?? null;
+  }
+
+  public generateInstallmentsAndSaveOrder(value: Date) {
+    let order: Order = { ...this._order, firstInstallmentDate: value };
+    const firstInstallmentDate = order.installments![0].paymentDate;
+
+    if (firstInstallmentDate && new Date(value) > new Date(firstInstallmentDate))
+      order = { ...this.orderService.generateInstallments(order, order.installments!.length) };
+
+    this.orderService.update(order).subscribe(order => {
+      this.snackBarService.success('Mudança(s) salva(s) com sucesso!');
+      this.saveOrderAction.emit({ ...order });
+    });
+  }
+
+  changeInstallments(value: Date) {
+    if (!this._order.installments?.length) {
+      this.generateInstallmentsAndSaveOrder(value);
+      return;
+    }
+
+    this.dialog.open(CustomDialogComponent, {
+      width: '250px',
+      data: {
+        title: "Refazer parcelas",
+        content: `Deseja refazer as parcelas?`,
+        onConfirmAction: () => this.generateInstallmentsAndSaveOrder(value),
+        onCancelAction: () => { }
+      }
+    });
+  }
+}
